f11dc984c0dd64fd2b03bd08aaa1bf20622a6e70
Pawan Kuamr Ranjan <kumarpawanranjan000@gmail.com>
2025-02-08 19:29:41 +0530

    UI for About, Contact, Appointment, MyAppointment, MyProfile, Login and Signup page

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -171,7 +171,7 @@ const Appointment = () => {
           ))}
         </div>
 
-        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4 scrollbar-hide'>
           {docSlots.length && docSlots[slotIndex].map((item, index) => (
             <p onClick={() => setSlotTime(item.time)} key={index} className={`text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-blue-500 text-white' : 'text-[#949494] border border-[#B4B4B4]'}`}>{item.time.toLowerCase()}</p>
           ))}
